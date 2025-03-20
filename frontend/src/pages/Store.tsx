4acb5eaa930bdab15bacdb88d361c3ad9730dcdb
David Etin <ajest123@gmail.com>
2025-03-20 15:31:42 +0200

    shwoing the items from the json file

--- frontend/src/pages/Store.tsx
+++ frontend/src/pages/Store.tsx
@@ -1,3 +1,16 @@
+import { StoreItem } from "../components/StoreItem";
+import storeItems from "../data/items.json";
+
 export function Store() {
-  return <div>Store</div>;
+  return (
+    <>
+      <div className="container  mx-auto px-4 mt-30">
+        <div className="grid md:grid-cols-3 gap-20 grid-rows-3 sm:grid-cols-1">
+          {storeItems.map((item) => {
+            return <StoreItem key={item.id} {...item} />;
+          })}
+        </div>
+      </div>
+    </>
+  );
 }
