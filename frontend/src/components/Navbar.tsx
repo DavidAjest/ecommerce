a9476efd8cc911eb350a62ef1580d4ac982784be
David Etin <ajest123@gmail.com>
2025-03-20 15:29:17 +0200

    added basic styles for Navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -26,14 +26,29 @@ export function Navbar() {
   );
 
   return (
-    <nav className="bg-gray-800 text-white fixed w-full top-0">
-      <div className="flex flex-row justify-between items-center max-w-9/10 mx-auto h-16">
-        <div>
-          <div>brand name</div>
+    <nav className="bg-gray-500 text-white fixed w-full top-0">
+      <div className="flex flex-row justify-between items-center max-w-9/10 mx-auto h-16 ">
+        <div className="flex flex-row items-center ">
+          <div className="font-bold">brand name</div>
+
+          <div className="px-10  ">
+            <button className="cursor-pointer flex flex-row items-center  bg-blue-500 h-12 w-12 rounded-full relative ">
+              <img
+                className="h-7 max-h-full mx-auto "
+                src="./cart.svg"
+                alt=""
+              />
+              <div className=" h-4 w-4 bg-pink-500 rounded-full absolute bottom-0 flex justify-center mx-auto items-center transform translate-x-0 translate-y-1">
+                2
+              </div>
+            </button>
+          </div>
         </div>
+
         <div className="hidden md:flex">{links}</div>
 
         <div className="md:hidden">
+          <div className="px-5">Cart</div>
           <button onClick={() => setIsOpen(!isOpen)}>icon</button>
         </div>
       </div>
