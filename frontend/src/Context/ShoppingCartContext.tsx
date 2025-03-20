d28c65bb3df8ec1adb7eb1c773689dbbf90ed478
David Etin <ajest123@gmail.com>
2025-03-20 15:23:15 +0200

    seconds commit

---
+++ frontend/src/Context/ShoppingCartContext.tsx
@@ -0,0 +1,48 @@
+import { createContext, useReducer, ReactNode } from "react";
+
+const ShoppingCartContext = createContext({});
+
+type CartItem = {
+  id: string;
+  name: string;
+  price: number;
+  quantity: number;
+};
+
+type ShoppingCartState = {
+  cartItems: CartItem[];
+};
+
+type ShopingCartAction = {
+  type: string;
+  payload: CartItem;
+};
+
+type ShoppingCartProviderProps = {
+  children: ReactNode;
+};
+
+const ShoppingCartReducer = (
+  state: ShoppingCartState,
+  action: ShopingCartAction
+) => {
+  switch (action.type) {
+    case "ADD_ITEM":
+      return { cartItems: [...state.cartItems, action.payload] };
+
+    default:
+      return state;
+  }
+};
+
+export const ShoppingCartProvider = ({
+  children,
+}: ShoppingCartProviderProps) => {
+  const [state, dispatch] = useReducer(ShoppingCartReducer, { cartItems: [] });
+
+  return (
+    <ShoppingCartContext.Provider value={{ ...state, dispatch }}>
+      {children}
+    </ShoppingCartContext.Provider>
+  );
+};
