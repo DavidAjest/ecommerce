7aac4beef3d96d15e9a99e74e50cf8612900ecbf
David Etin <ajest123@gmail.com>
2025-03-20 15:27:54 +0200

    added tag of shoppingcartprovider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,18 +6,21 @@ import { Home } from "./pages/Home";
 import { About } from "./pages/About";
 import { Store } from "./pages/Store";
 import { Navbar } from "./components/Navbar";
+import { ShoppingCartProvider } from "./Context/ShoppingCartContext";
 
 function App() {
   return (
     <>
       <Navbar />
-      <div className="mb-4">
-        <Routes>
-          <Route path="/home" element={<Home />} />
-          <Route path="/store" element={<Store />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </div>
+      <ShoppingCartProvider>
+        <div className=" container mx-auto mb-4">
+          <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route path="/store" element={<Store />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
+      </ShoppingCartProvider>
     </>
   );
 }
