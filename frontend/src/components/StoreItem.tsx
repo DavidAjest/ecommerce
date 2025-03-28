e25f3b4365a5143d245e178ddc66f46dbd279acc
David Etin <ajest123@gmail.com>
2025-03-28 13:44:17 +0300

    Implement quantity management for store items in the shopping cart

--- frontend/src/components/StoreItem.tsx
+++ frontend/src/components/StoreItem.tsx
@@ -1,13 +1,37 @@
+import useShoppingCart from "../Hooks/useShoppingCart";
 type StoreItemPorps = {
   id: number;
   name: string;
   price: number;
   imgUrl: string;
+  quantity: number;
 };
 
-export function StoreItem({ id, name, price, imgUrl }: StoreItemPorps) {
-  const quantity = 0;
-  const handleAddToCart = () => {};
+export function StoreItem({
+  id,
+  name,
+  price,
+  imgUrl,
+}: // quantity,
+StoreItemPorps) {
+  const { state, dispatch } = useShoppingCart();
+  const { cartItems } = state;
+
+  const handleIncreaseAmount = () => {
+    dispatch({
+      type: "INCREASE_AMOUNT",
+      payload: { id, name, price, imgUrl },
+    });
+  };
+
+  const handleDecreaseAmount = () => {
+    if (cartItems.find((item) => item.id === id && item.quantity === 1)) {
+      dispatch({ type: "REMOVE_ITEM_FROM_CART", payload: { id } });
+    } else {
+      dispatch({ type: "DECREASE_AMOUNT", payload: { id } });
+    }
+  };
+  // console.log("this is inssdsdsdsded REMOVE", cartItems);
   return (
     <div className="flex flex-col bg-orange-500 overflow-hidden rounded-lg shadow-md ">
       <div className="overflow-hidden">
@@ -27,28 +51,40 @@ export function StoreItem({ id, name, price, imgUrl }: StoreItemPorps) {
         <span className="font-bold text-1xl block text-gray-700 py-3 px-3">
           13$
         </span>
+        {/*  */}
         <div className="flex flex-row justify-center mt-auto ">
-          {quantity === 0 ? (
+          {!cartItems.find(
+            (item) => item.id === id && (item.quantity || 0) > 0
+          ) ? (
             <button
-              onClick={() => handleAddToCart()}
-              className="cursor-pointer ml-5 bg-green-500 w-full  rounded-lg"
+              onClick={() => handleIncreaseAmount()}
+              className="cursor-pointer ml-5 bg-green-500 w-full rounded-lg"
             >
               Add To Cart
             </button>
           ) : (
             <div>
-              <button className="cursor-pointer  bg-green-500 size-10 rounded-lg">
+              <button
+                onClick={() => handleDecreaseAmount()}
+                className="cursor-pointer  bg-green-500 size-10 rounded-lg"
+              >
                 -1
               </button>
               <span className="cursor-pointer ml-5 bg-green-500 size-10 rounded-lg">
-                0
+                {cartItems.map((item) => {
+                  if (item.id === id) return item.quantity;
+                })}
               </span>
-              <button className="cursor-pointer ml-5 bg-green-500 size-10 rounded-lg">
+              <button
+                onClick={() => handleIncreaseAmount()}
+                className="cursor-pointer ml-5 bg-green-500 size-10 rounded-lg"
+              >
                 +1
               </button>
             </div>
           )}
         </div>
+        {/*  */}
       </div>
     </div>
   );
