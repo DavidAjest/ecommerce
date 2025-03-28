775935a12cfa1f4b2af05ff1e3b9bc788419d3da
David Etin <ajest123@gmail.com>
2025-03-28 13:44:40 +0300

    Enhance shopping cart functionality with quantity management and item removal

--- frontend/src/Context/ShoppingCartContext.tsx
+++ frontend/src/Context/ShoppingCartContext.tsx
@@ -1,13 +1,13 @@
 import { createContext, useReducer, ReactNode } from "react";
+import { CartItem } from "../components/CartItem";
 
-const ShoppingCartContext = createContext({});
-
-type CartItem = {
-  id: string;
+type CartItem = Partial<{
+  id: string | number;
   name: string;
   price: number;
   quantity: number;
-};
+  imgUrl: string;
+}>;
 
 type ShoppingCartState = {
   cartItems: CartItem[];
@@ -22,15 +22,54 @@ type ShoppingCartProviderProps = {
   children: ReactNode;
 };
 
+type ContextType = {
+  state: ShoppingCartState;
+  dispatch: React.Dispatch<ShopingCartAction>;
+};
+
+export const ShoppingCartContext = createContext<ContextType | undefined>(
+  undefined
+);
+
 const ShoppingCartReducer = (
   state: ShoppingCartState,
   action: ShopingCartAction
 ) => {
   switch (action.type) {
-    case "ADD_ITEM":
-      return { cartItems: [...state.cartItems, action.payload] };
-    case "REMOVE_ITEM":
-      return { cartItems: [...state.cartItems, action.payload] };
+    case "INCREASE_AMOUNT": {
+      if (!state.cartItems.find((item) => item.id === action.payload.id)) {
+        return {
+          ...state,
+          cartItems: [...state.cartItems, { ...action.payload, quantity: 1 }],
+        };
+      }
+
+      const newCartItems = state.cartItems.map((item) => {
+        if (item.id === action.payload.id) {
+          return { ...item, quantity: (item.quantity || 0) + 1 };
+        } else {
+          return item;
+        }
+      });
+      return { ...state, cartItems: newCartItems };
+    }
+    case "DECREASE_AMOUNT": {
+      const newCartItems = state.cartItems.map((item) => {
+        if (item.id === action.payload.id && (item.quantity || 0) > -1) {
+          return { ...item, quantity: (item.quantity || 0) - 1 };
+        } else {
+          return item;
+        }
+      });
+      return { ...state, cartItems: newCartItems };
+    }
+    case "REMOVE_ITEM_FROM_CART":
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          (item) => item.id !== action.payload.id
+        ),
+      };
     case "GET_ITEM":
       return { cartItems: [...state.cartItems, action.payload] };
 
@@ -42,10 +81,13 @@ const ShoppingCartReducer = (
 export const ShoppingCartProvider = ({
   children,
 }: ShoppingCartProviderProps) => {
-  const [state, dispatch] = useReducer(ShoppingCartReducer, { cartItems: [] });
+  const [state, dispatch] = useReducer(ShoppingCartReducer, {
+    cartItems: [],
+  });
 
+  console.log(state);
   return (
-    <ShoppingCartContext.Provider value={{ ...state, dispatch }}>
+    <ShoppingCartContext.Provider value={{ state, dispatch }}>
       {children}
     </ShoppingCartContext.Provider>
   );
