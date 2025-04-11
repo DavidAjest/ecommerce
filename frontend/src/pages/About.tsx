a137af9400d2a0ad849d40ab85cb915e9dbe7ad8
David Etin <ajest123@gmail.com>
2025-04-11 14:05:52 +0300

    added the about page

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -1,3 +1,28 @@
 export function About() {
-  return <div>About</div>;
+  return (
+    <div>
+      uAbot Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab,
+      ducimus explicabo est nemo assumenda sequi at accusantium quo
+      consequuntur. Porro qui magnam vel suscipit eaque soluta laudantium
+      tempora ipsum impedit!
+      <div>
+        uAbot Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab,
+        ducimus explicabo est nemo assumenda sequi at accusantium quo
+        consequuntur. Porro qui magnam vel suscipit eaque soluta laudantium
+        tempora ipsum impedit!
+      </div>
+      <div>
+        uAbot Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab,
+        ducimus explicabo est nemo assumenda sequi at accusantium quo
+        consequuntur. Porro qui magnam vel suscipit eaque soluta laudantium
+        tempora ipsum impedit!
+      </div>
+      <div>
+        uAbot Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab,
+        ducimus explicabo est nemo assumenda sequi at accusantium quo
+        consequuntur. Porro qui magnam vel suscipit eaque soluta laudantium
+        tempora ipsum impedit!
+      </div>
+    </div>
+  );
 }
